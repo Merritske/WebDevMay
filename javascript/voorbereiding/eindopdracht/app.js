c494109fc35c1487e7103915b035c884b74e07b8
merritske <merritske@gmail.com>
2022-06-27 10:51:40 +0200

    zoekfunctie

--- javascript/voorbereiding/eindopdracht/app.js
+++ javascript/voorbereiding/eindopdracht/app.js
@@ -26,63 +26,95 @@ document.getElementById("introForm").style.display = "none"
  let blogInhoud = document.querySelectorAll(".blogInhoud");
 let blogTitel = document.querySelectorAll(".blogTitel");
 let blog = document.querySelector(".blog")
-// let toonBlog = false;
-function showBlog(e){
- //  toonBlog = true;
-console.log(e.firstChild.nextElementSibling.innerHTML)
-     for(let x = 0 ; x<blogTitel.length; x++){  
-         console.log(blogTitel[x].innerHTML)
-    if(blogTitel[x].innerHTML == e.firstChild.nextElementSibling.innerHTML){
 
-        // blogInhoud[x].style.display = "block"
-         blogInhoud[x].style.visibility = "visible"
-    }else{
-         //blogInhoud[x].style.display = "none";
-         blogInhoud[x].style.visibility = "hidden"
-}
-    }
- }
+// function showBlog(e){
+// console.log(e.firstChild.nextElementSibling.innerHTML)
+//      for(let x = 0 ; x<blogTitel.length; x++){  
+//          console.log(blogTitel[x].innerHTML)
+//     if(blogTitel[x].innerHTML == e.firstChild.nextElementSibling.innerHTML){
+
+//          blogInhoud[x].style.display = "block"
+//        //  blogInhoud[x].style.visibility = "visible"
+//     }else{
+//          blogInhoud[x].style.display = "none";
+//         // blogInhoud[x].style.visibility = "hidden"
+// }
+//     }
+//  }
 
 //  //OFWEL DIT = UITBREIDING VAN DE OEFENING
-//    let blogArray = [];
-// window.addEventListener("DOMContentLoaded", function(){
-//    blogArray = JSON.parse(localStorage.getItem("blog"))
-//    blogArray.map((item)=>{
-//     blog.innerHTML +=  `<li onclick="showBlog(this)">
-//     <h3  class="blogTitel">${item.blogTitel} </h3> 
-//     <p class="blogInhoud">${item.blogInhoud}</p>
-// </li> ` 
-//    })
+   let blogArray = [];  
+    let inputBlogTitel = document.getElementById("inputBlogTitel")
+ let inputBlogTekst = document.getElementById("inputBlogTekst")
 
-// })
+function blogOpslaan(){
+    console.log(introInput.value)
+    if(introInput.value == ""){
+        alert("geef je naam")
+    }else{
+            blog.innerHTML += `<li onclick="showBlog(this)">
+    <h3  class="blogTitel">${inputBlogTitel.value} </h3> 
+    <p class="blogInhoud">${inputBlogTekst.value}</p>
+</li> ` 
+console.log(inputBlogTekst.value)
+    blogArray.push({
+    blogTitel: inputBlogTitel.value,
+    blogInhoud: inputBlogTekst.value
+}) 
+console.log(blogArray)
+    localStorage.setItem("blog", JSON.stringify(blogArray))
+    }
 
-//    function showBlog(e){
-//   for(let x = 0; x< blog.children.length; x++){  
-//     console.log(blog.children[x].firstChild.nextElementSibling.innerHTML)
-//       if(e.firstChild.nextElementSibling.innerHTML == blog.children[x].firstChild.nextElementSibling.innerHTML){
-//      blog.children[x].lastChild.previousSibling.style.display = "block"
-//       }else{
-//         blog.children[x].lastChild.previousSibling.style.display = "none"
-//      }
-//  }
-// }
+console.log(blogArray)
+inputBlogTitel.value = "";
+inputBlogTekst.value = ""
+}
+window.addEventListener("DOMContentLoaded", function(){
+       console.log(blogArray.length)    
+       blogArray = JSON.parse(localStorage.getItem("blog"))
+if(blogArray.length != 0){ 
+   
+  blogArray.map((item)=>{
+     console.log(item.blogInhoud)
+    blog.innerHTML +=  `<li onclick="showBlog(this)">
+    <h3  class="blogTitel">${item.blogTitel} </h3> 
+    <p class="blogInhoud">${item.blogInhoud}</p>
+</li> ` 
+   })
+}else if(blogArray.length == 0 ){
+    console.log("hoep")
+   alert("voeg iets in in de blog")
+}
+ console.log(blogArray)
+  
+})
 
-//    let inputBlogTitel = document.getElementById("inputBlogTitel")
-//  let inputBlogTekst = document.getElementById("inputBlogTekst")
+   function showBlog(e){
+  console.log(introInput.value)
+  for(let x = 0; x< blog.children.length; x++){  
+    console.log(blog.children[x].lastChild.previousSibling.innerHTML)
+      if(e.firstChild.nextElementSibling.innerHTML == blog.children[x].firstChild.nextElementSibling.innerHTML){
+     blog.children[x].lastChild.previousSibling.style.display = "block"
+      }else{
+        blog.children[x].lastChild.previousSibling.style.display = "none"
+     }
+ }
+}   
 
-// function blogOpslaan(){
-//     blog.innerHTML += `<li onclick="showBlog(this)">
-//     <h3  class="blogTitel">${inputBlogTitel.value} </h3> 
-//     <p class="blogInhoud">${inputBlogTekst.value}</p>
-// </li> ` 
-
-// blogArray = [...blogArray, {
-//     blogTitel: inputBlogTitel.value,
-//     blogInhoud: inputBlogTekst.value
-// }]
-// localStorage.setItem("blog", JSON.stringify(blogArray))
-// console.log(blogArray)
-// inputBlogTitel.value = "";
-// inputBlogTekst.value = ""
-// }
+let zoek = document.getElementById("zoek")
+let zoekresultaat = document.getElementById("zoekresultaat")
+function zoekBlog(){
+   
+    for(let z = 0; z< blogArray.length;z++){ 
+    let b= blogArray[z].blogTitel ;  
+    console.log(b)    
+     let a = zoek.value;
+     console.log(b.includes(a))  
+       if(b.includes(a)){
+           console.log(blog.children[z])
+           blog.children[z].style.display = "block"       
+         }else{
+            blog.children[z].style.display = "none" } 
+    }
+}
 
