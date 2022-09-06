cb6a8b205cf27d726b467280d5b5e129dd445b71
merritske <merritske@gmail.com>
2022-09-06 14:06:38 +0200

    userController

--- javascript/classes/userController.js
+++ javascript/classes/userController.js
@@ -3,8 +3,8 @@
 class User {
     constructor(username, password) {
         this.username = username;
-        this.password = password; 
-    } 
+        this.password = password;
+    }
 }
 let users = []
 
@@ -12,9 +12,9 @@ class Guest extends User {
     constructor(username, password, permission) {
         super(username, password);
         this.permission = permission;
-      
+
     }
- register() {
+    register() {
         users.push({
             username: this.username,
             password: this.password,
@@ -27,9 +27,9 @@ class Admin extends User {
     constructor(username, password, permission) {
         super(username, password);
         this.permission = permission;
-      
+
     }
- register() {
+    register() {
         users.push({
             username: this.username,
             password: this.password,
@@ -46,4 +46,87 @@ app2.register();
 let app3 = new Guest("Pol", "popo321", "Guest")
 app3.register();
 //console.log(app, app2, app3)
+let app4 = new Guest('Jacques', 'polle56', 'Guest')
+app4.register()
 console.log(users)
+console.log(users[2])
+users.forEach(user => {
+    console.log(user.username)
+})
+
+//studentlist
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+let students = [];
+class Student extends Person {
+    constructor(role, name, age) {
+        super(name, age);
+        this.role = role;
+    }
+    addStudent() {
+        return students.push({
+            name: this.name,
+            age: this.age
+        })
+    }
+}
+
+let teachers = []
+class Teacher extends Person {
+    constructor(role, name, age) {
+        super(name, age);
+        this.role = role;
+    }
+    addTeachers() {
+        return teachers.push({
+            name: this.name,
+            age: this.age
+        })
+    }
+}
+let stud1 = new Student('student', 'Jef', '25')
+console.log(stud1.name)
+stud1.addStudent()
+let stud2 = new Student('student', 'Louis', '22')
+stud2.addStudent()
+console.log(students)
+
+//andere oplossing
+class Client {
+    constructor(role = 'user') {
+        this.role = role;
+    }
+}
+
+class Controller extends Client {
+    constructor(role) {
+        super(role);
+        this.users = [];
+    }
+
+    getUsers() {
+        return this.users;
+    }
+
+    register(username, password) {
+        this.users.push({
+            username,
+            password,
+            role: this.role
+        })
+    }
+}
+
+
+let admin = new Controller('Administrator');
+admin.register('mehmet', 'mehmet12345');
+admin.register('ali', 'ali12345');
+// console.log(admin.getUsers())
+
+admin.getUsers().forEach(user => {
+    console.log(user.username)
+})
