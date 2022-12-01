6ffd9639bb37591ff89b0e0378be45d140a3d38a
merritske <merritske@gmail.com>
2022-12-01 15:16:07 +0100

    aanpassing quiz app

--- reactjs/quiz/src/components/QuizContainer.js
+++ reactjs/quiz/src/components/QuizContainer.js
@@ -2,34 +2,31 @@ import React, { useState, useEffect } from 'react'
 
 function QuizContainer() {
   const [questions, setQuestions]= useState([])
-  const [allAnswers, setAllAnswers] = useState([])
-const [currentQuestion, setCurrentQuestion] = useState(0)
-  useEffect(()=>{
+ // const [allAnswers, setAllAnswers] = useState([])
+//const [currentQuestion, setCurrentQuestion] = useState(0)
+
+useEffect(()=>{
     async   function fetchQuestions(){
       console.log('load')
           let res = await fetch('https://the-trivia-api.com/api/questions?categories=arts_and_literature&limit=15&region=BE&difficulty=easy')
        let data = await res.json()
            console.log(data)
-           data.map((q)=>{console.log(q.question)
-         
-        })
-
   setQuestions(data.map((q)=>q))
           }
-              
-        
-           
-        
+
           fetchQuestions()
-         // setAllAnswers(questions[0].correctAnswer)
-        },[])
+        
+        },[]) 
+           console.log(questions[0].question)
+       // setAllAnswers(questions[currentQuestion].correctAnswer)
     return (
     <div>
        <h2> QuizContainer</h2>
        <div style={{'display': 'flex'}}>
 <p>Question:   
-{   console.log(questions[0].question)}
+{/*  */}
      </p>
+    {/* <h3>{allAnswers}</h3> */}
 <h4> SCORE : </h4>
        </div>
 
