a89821f32dee53885995a7e6518f4b040b76ad37
merritske <merritske@gmail.com>
2023-01-20 15:19:55 +0100

    CMS not good

---
+++ oefeningen/CMS/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+let activiteiten  = document.querySelector('#activiteit')
+
+activiteiten.innerHTML = './text.txt'
+
+fs.readFile('./text.txt', 'utf-8', (err, data)=>{
+    console.log(data)
+})
